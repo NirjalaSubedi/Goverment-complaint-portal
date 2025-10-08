f42c17826de56ea9bae928f57b0e87a835bdfd6e
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-10-08 19:49:14 +0545

    Enhance language translation support by adding missing text elements and updating HTML structure for better accessibility

--- LanguageToggle.js
+++ LanguageToggle.js
@@ -5,8 +5,13 @@ const Translate={
         title:'Goverment Complaint Portal',
         Features:'Features',
         HowItWorks:'How It Works',
-        Contact:'Contact'
-
+        Contact:'Contact',
+        login:'Login',
+        topic:'Make Your Voice',
+        heard:'Heard',
+        desc:' Report corruption,road damage,water supply issues and other problems.we listen to your voice and take action.',
+        submitComplaint:'Submit Complaint',
+        registerbtn:'Register',
 
     },
     'ne':{
@@ -14,16 +19,27 @@ const Translate={
         title:'सरकारी उजुरी पोर्टल',
         Features:'विशेषताहरु',
         HowItWorks:'यो कसरी काम गर्छ',
-        Contact:'सम्पर्क'
+        Contact:'सम्पर्क',
+        login:'लगइन',
+        topic:'आफ्नो आवाज बनाउनुहोस्',
+        heard:'सुनियो',
+        desc:'भ्रष्टाचार, सडक क्षति, पानी आपूर्ति समस्या र अन्य समस्याहरू रिपोर्ट गर्नुहोस्। हामी तपाईंको आवाज सुन्छौं र कारबाही गर्छौं।',
+        submitComplaint:'उजुरी पेश गर्नुहोस्',
+        registerbtn:'दर्ता गर्नुहोस्',
     }
 }
 function LanguageTranslate(){
     currentLanguage=currentLanguage==='en'?'ne':'en';
     document.getElementById('language-toggle').innerText=Translate[currentLanguage].languageToggle;
-    document.getElementById('title').innerText=Translate[currentLanguage].title;
+    document.getElementsByClassName('Heading1')[0].innerText=Translate[currentLanguage].title;
     document.getElementById('Features').innerText=Translate[currentLanguage].Features;
     document.getElementById('HowItWorks').innerText=Translate[currentLanguage].HowItWorks;
     document.getElementById('Contact').innerText=Translate[currentLanguage].Contact;
-
+    document.getElementById('login-btn').innerText=Translate[currentLanguage].login;
+    document.getElementsByClassName('Heading1')[1].innerText=Translate[currentLanguage].topic;
+    document.getElementById('Heard').innerText=Translate[currentLanguage].heard;
+    document.getElementsByClassName('desc')[0].innerText=Translate[currentLanguage].desc;
+    document.getElementsByClassName('submit-complaint')[0].innerText=Translate[currentLanguage].submitComplaint;
+    document.getElementsByClassName('register-btn')[0].innerText=Translate[currentLanguage].registerbtn;
 
 }
