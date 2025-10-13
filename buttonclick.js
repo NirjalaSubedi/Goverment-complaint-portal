3c459cb94f422eadd68c79542c8980083847326e
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-10-13 19:55:34 +0545

    Refactor button click handling for register button and improve login button functionality

--- buttonclick.js
+++ buttonclick.js
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded',function(){
-const registerbtn=document.getElementById('register-btn')||document.getElementById('Register-btn');
+const loginbtn=document.getElementById('Login-btn');
+const registerbtn=document.getElementById('Register-btn');
+
+function disableloginbtn(){
+	document.getElementsByClassName('email')[0].disabled=true;
+	document.getElementById('email').disabled=true;
+	document.getElementById('password').disabled=true;
+}
 if(registerbtn){
 	registerbtn.addEventListener('click',function(e){
 		e.preventDefault();
 		registerbtn.style.backgroundColor='#0d55e7';
 		registerbtn.style.color='white';
+		disableloginbtn();
 	})
 }
 })
