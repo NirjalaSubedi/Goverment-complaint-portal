c2dbc26ac8b53e6d4d7f8d60cca43fb2d7e1881c
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-11-13 20:11:31 +0545

    Refactor complaint submission handling: organize POST data retrieval

--- complaint_submit.php
+++ complaint_submit.php
@@ -62,6 +62,12 @@ include ("./backend/databaseConnection.php");
 
 <?php
     if($_POST['submit']){
-        
+        $complaintType= $_POST['complaint-type'];
+        $subject= $_POST['subject'];
+        $Location= $_POST['location'];
+        $priorityLevel= $_POST['priorityLevel'];
+        $description= $_POST['description'];
+        $attachment= $_POST['attachment'];
+
     }
  ?>
