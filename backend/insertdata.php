ed682249377d8ae0165a296913321c30267e2f95
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-11-14 06:47:32 +0545

    Add complaint submission form and backend handling: implement new HTML form and update PHP processing

--- backend/insertdata.php
+++ backend/insertdata.php
@@ -1,4 +1,13 @@
 <?php
     include 'databaseConnection.php';
+    if($_POST['submit']){
+        $complaintType= $_POST['complaint-type'];
+        $subject= $_POST['subject'];
+        $Location= $_POST['location'];
+        $priorityLevel= $_POST['priorityLevel'];
+        $description= $_POST['description'];
+        $attachment= $_POST['attachment'];
+
+    }
     
  ?>
