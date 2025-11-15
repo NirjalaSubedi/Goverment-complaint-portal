e955aa9cc1a8a255ef10eae233477c799ed5c677
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-11-15 19:39:11 +0545

    Refactor code structure for improved readability and maintainability

--- frontend/script.js
+++ frontend/script.js
@@ -2,3 +2,7 @@
 document.getElementById('login-btn').addEventListener('click',function(){
     window.location.href='login.html';
 })
+
+document.getElementById('viewDetailsBtn').addEventListener('click',function(){
+    window.location.href='Viewderails.html';
+})
