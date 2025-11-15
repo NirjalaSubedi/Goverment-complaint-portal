af61e9c0f3f19f1cf2957697ed42f05b37b42f4b
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-11-15 19:45:29 +0545

    Update navigation links in complaint submission and dashboard pages for consistency

--- frontend/script.js
+++ frontend/script.js
@@ -3,6 +3,3 @@ document.getElementById('login-btn').addEventListener('click',function(){
     window.location.href='login.html';
 })
 
-document.getElementById('viewDetailsBtn').addEventListener('click',function(){
-    window.location.href='Viewderails.html';
-})
