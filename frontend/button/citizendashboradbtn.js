16fd638ef13737b02a6edb6fdd40aac78b2535c0
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-11-21 19:38:27 +0545

    Add citizen dashboard button functionality and update script reference in HTML

---
+++ frontend/button/citizendashboradbtn.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const mycomplaints=document.getElementById('myComplaints-btn');
+    const drafts=document.getElementById('myDrafts');
+    const profile=document.getElementById('see-profile');
+
+    const mycomplaintsField=[
+        document.getElementById("myComplaintsText"),
+        document.getElementById('complainttopdesc'),
+        document.getElementById('addtitle'),
+        
+    ]
+
+});
