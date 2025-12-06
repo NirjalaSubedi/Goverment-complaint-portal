b28e806db9c433c6ef771bdcf4422c16efe8906d
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-12-06 12:51:27 +0545

    Refactor user creation logic in backend and update login form structure for improved clarity

--- backend/createuser.php
+++ backend/createuser.php
@@ -1,15 +1,16 @@
 <?php
 include '../includes/databaseConnection.php';
-if($_POST['submit']){
-    $fullname=$_POST['fullName'];
-    $phonenumber=$_POST['phonenumber'];
-    $email=$_POST['email'];
-    $address=$_POST['address'];
-    $department=$_POST['department'];
-    $citizenship=$_POST['citizenshipNumber'];
-    $officerId=$_FILES['officerId']['name'];
-    $password=$_POST['password'];
-    $confirmPassword=$_POST['confirmPassword'];
+if (isset($_POST['submit'])) {
+    $fullname = $_POST['fullName'];
+    $phonenumber = $_POST['phonenumber'];
+    $email = $_POST['email']; 
+    $citizenship = $_POST['citizenshipNumber'];
+    $sql = "INSERT INTO users(full_name, phone_number, email,  citizenshipnumber) 
+            VALUES ('$fullname', '$phonenumber', '$email', '$citizenship')";
+    if (mysqli_query($conn, $sql)) {
+        echo "User created successfully";
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
 }
 ?>
-
