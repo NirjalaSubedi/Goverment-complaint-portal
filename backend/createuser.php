28a4d4d45ce2a90d8e08301d1d8dc5db451902cc
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-12-10 06:52:23 +0545

    Enhance user creation process by adding password hashing and approval status; update login form to include hidden user type input

--- backend/createuser.php
+++ backend/createuser.php
@@ -4,9 +4,14 @@ if (isset($_POST['submit'])) {
     $fullname = $_POST['fullName'];
     $phonenumber = $_POST['phonenumber'];
     $email = $_POST['email']; 
-    $citizenship = $_POST['citizenshipNumber'];
-    $sql = "INSERT INTO users(full_name, phone_number, email,  citizenshipnumber) 
-            VALUES ('$fullname', '$phonenumber', '$email', '$citizenship')";
+    $citizenship = $_POST['citizenshipNumber']; 
+    $password = $_POST['password']; 
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    $user_type = 'Citizen'; 
+    $is_approved = 'Approved'; 
+    $sql = "INSERT INTO users(full_name, phone_number, email, citizenship_number, password_hash, user_type, is_approved) 
+            VALUES ('$fullname', '$phonenumber', '$email', '$citizenship', '$hashed_password', '$user_type', '$is_approved')";
+    
     if (mysqli_query($conn, $sql)) {
         echo "User created successfully";
     } else {
