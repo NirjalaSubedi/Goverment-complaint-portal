fcca691a52c2012f07fec4a18ea0ee394923907e
Nirjala Subedi <nirjalasubedi944@gmail.com>
2025-12-23 17:55:29 +0545

    Add getuser.php to return user information from session

---
+++ backend/getuser.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    echo json_encode(['error' => 'Not logged in']);
+    exit;
+}
+// Return user information from session
+$userInfo = [
+    'fullName' => $_SESSION['user_name'] ?? 'User',
+    'email' => $_SESSION['user_email'] ?? '',
+    'userType' => $_SESSION['user_type'] ?? 'Citizen',
+    'userId' => $_SESSION['user_id'] ?? 0
+];
+
+header('Content-Type: application/json');
+echo json_encode($userInfo);
+?>
